e1dfee4cb0d514641ba0edd069b0fd92ba26af5d
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2021-10-10 18:24:24 -0500

    updated to have JWT expire after 30 minutes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '1800s' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
